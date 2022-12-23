819adbc0c3e63fa4e77c190b604b94e011568c90
nlessmann <773597+nlessmann@users.noreply.github.com>
2022-12-23 20:16:43 +0100

    Solution for day 22 part 1 and day 23

---
+++ Day22/Program.cs
@@ -0,0 +1,221 @@
+﻿namespace Day22
+{
+    using System.Text.RegularExpressions;
+    
+    internal class Map
+    {
+        private enum Tile
+        {
+            Void,
+            Open,
+            Wall
+        }
+
+        private enum Facing
+        {
+            Right = 0,
+            Down = 1,
+            Left = 2,
+            Up = 3
+        }
+
+        private readonly record struct Position(int Row, int Column, Facing Facing);
+
+        private readonly int _width;
+        private readonly int _height;
+        private readonly Tile[,] _map;
+
+        private Position _position;
+
+        public Map(IReadOnlyList<string> scan)
+        {
+            // Construct map from scan
+            _width = scan.Max(s => s.Length);
+            _height = scan.Count;
+            _map = new Tile[_height, _width];
+
+            bool startingPositionFound = false;
+
+            for (int row = 0; row < _height; row++)
+            {
+                string line = scan[row];
+                for (int col = 0; col < line.Length; col++)
+                {
+                    _map[row, col] = line[col] switch
+                    {
+                        '.' => Tile.Open,
+                        '#' => Tile.Wall,
+                        _ => Tile.Void
+                    };
+
+                    if (!startingPositionFound && _map[row, col] == Tile.Open)
+                    {
+                        _position = new Position(row, col, Facing.Right);
+                        startingPositionFound = true;
+                    }
+                }
+            }
+        }
+
+        public int FollowPath(string path)
+        {
+            // Split parse into individual moves
+            string[] moves = Regex.Matches(path, @"(\d+|R|L)").Select(m => m.Value).ToArray();
+
+            // Perform all moves
+            foreach (string move in moves)
+            {
+                if (move == "R")
+                {
+                    Facing f = _position.Facing;
+                    _position = _position with
+                    {
+                        Facing = f == Facing.Up ? Facing.Right : f + 1
+                    };
+                }
+                else if (move == "L")
+                {
+                    Facing f = _position.Facing;
+                    _position = _position with
+                    {
+                        Facing = f == Facing.Right ? Facing.Up : f - 1
+                    };
+                }
+                else
+                {
+                    int steps = int.Parse(move);
+                    for (int i = 0; i < steps; i++)
+                    {
+                        Position candidate = _position.Facing switch
+                        {
+                            Facing.Right => TileOnTheRight(),
+                            Facing.Down => TileBelow(),
+                            Facing.Left => TileOnTheLeft(),
+                            Facing.Up => TileAbove(),
+                            _ => throw new ArgumentException("Invalid path")
+                        };
+                        
+                        // Are we standing in front of a wall?
+                        if (_map[candidate.Row, candidate.Column] == Tile.Wall)
+                        {
+                            break;
+                        }
+
+                        _position = candidate;
+                    }
+                }
+            }
+            
+            // Compute score = final password
+            return 1000 * (_position.Row + 1) + 4 * (_position.Column + 1) + (int)_position.Facing;
+        }
+
+        private Position TileOnTheRight()
+        {
+            int col = _position.Column + 1;
+            if (col >= _width || _map[_position.Row, col] == Tile.Void)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    if (_map[_position.Row, c] != Tile.Void)
+                    {
+                        col = c;
+                        break;
+                    }
+                }
+            }
+
+            return _position with { Column = col };
+        }
+
+        private Position TileOnTheLeft()
+        {
+            int col = _position.Column - 1;
+            if (col < 0 || _map[_position.Row, col] == Tile.Void)
+            {
+                for (int c = _width - 1; c > col; c--)
+                {
+                    if (_map[_position.Row, c] != Tile.Void)
+                    {
+                        col = c;
+                        break;
+                    }
+                }
+            }
+
+            return _position with { Column = col };
+        }
+
+        private Position TileBelow()
+        {
+            int row = _position.Row + 1;
+            if (row >= _height || _map[row, _position.Column] == Tile.Void)
+            {
+                for (int r = 0; r < row; r++)
+                {
+                    if (_map[r, _position.Column] != Tile.Void)
+                    {
+                        row = r;
+                        break;
+                    }
+                }
+            }
+
+            return _position with { Row = row };
+        }
+
+        private Position TileAbove()
+        {
+            int row = _position.Row - 1;
+            if (row < 0 || _map[row, _position.Column] == Tile.Void)
+            {
+                for (int r = _height - 1; r > row; r--)
+                {
+                    if (_map[r, _position.Column] != Tile.Void)
+                    {
+                        row = r;
+                        break;
+                    }
+                }
+            }
+
+            return _position with { Row = row };
+        }
+    }
+    
+    public class Puzzle
+    {
+        private readonly Map _map;
+        private readonly string _path;
+        
+        private Puzzle(string inputFile)
+        {
+            string[] input = File.ReadAllLines(inputFile);
+            _map = new Map(input.TakeWhile(line => line.Trim() != "").ToArray());
+            _path = input[^1].Trim();
+        }
+
+        private int ComputeSolution1()
+        {
+            return _map.FollowPath(_path);
+        }
+
+        private int ComputeSolution2()
+        {
+            return 0;
+        }
+        
+        public static void Main()
+        {
+            var test = new Puzzle(@"D:\Advent\advent-of-csharp-2022\Day22\TestInput.txt");
+            Console.WriteLine($"Test solution 1: {test.ComputeSolution1()} (should be 6032)");
+            //Console.WriteLine($"Test solution 2: {test.ComputeSolution2()} (should be ?)");
+            
+            Console.WriteLine("---------------");
+            
+            var puzzle = new Puzzle(@"D:\Advent\advent-of-csharp-2022\Day22\PuzzleInput.txt");
+            Console.WriteLine($"Puzzle solution 1: {puzzle.ComputeSolution1()}");
+            //Console.WriteLine($"Puzzle solution 2: {puzzle.ComputeSolution2()}");
+        }
+    }
+}
---
+++ Day23/Program.cs
@@ -0,0 +1,185 @@
+﻿namespace Day23
+{
+    internal class Grove
+    {
+        private enum Direction
+        {
+            North,
+            South,
+            West,
+            East
+        }
+
+        private static Direction NextDirection(Direction direction)
+        {
+            direction += 1;
+            return Enum.IsDefined(direction) ? direction : 0;
+        }
+
+        private readonly record struct Elf(int X, int Y)
+        {
+            public IEnumerable<Elf> Neighbors()
+            {
+                for (int dx = -1; dx <= 1; dx++)
+                {
+                    for (int dy = -1; dy <= 1; dy++)
+                    {
+                        if (dx != 0 || dy != 0)
+                        {
+                            yield return new Elf(X + dx, Y + dy);
+                        }
+                    }
+                }
+            }
+
+            public IEnumerable<Elf> Neighbors(Direction direction)
+            {
+                switch (direction)
+                {
+                    case Direction.North:
+                        yield return this with { Y = Y - 1 };
+                        yield return new Elf(X - 1, Y - 1);
+                        yield return new Elf(X + 1, Y - 1);
+                        break;
+
+                    case Direction.South:
+                        yield return this with { Y = Y + 1 };
+                        yield return new Elf(X - 1, Y + 1);
+                        yield return new Elf(X + 1, Y + 1);
+                        break;
+
+                    case Direction.West:
+                        yield return this with { X = X - 1 };
+                        yield return new Elf(X - 1, Y + 1);
+                        yield return new Elf(X - 1, Y - 1);
+                        break;
+                    
+                    case Direction.East:
+                        yield return this with { X = X + 1 };
+                        yield return new Elf(X + 1, Y + 1);
+                        yield return new Elf(X + 1, Y - 1);
+                        break;
+                    
+                    default:
+                        throw new ArgumentException("Invalid direction");
+                }
+            }
+        }
+
+        private readonly HashSet<Elf> _elves = new();
+        private Direction _direction;
+        
+        public int Rounds { get; private set; }
+
+        public Grove(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            for (int row = 0; row < lines.Length; row++)
+            {
+                string line = lines[row];
+                for (int col = 0; col < line.Length; col++)
+                {
+                    if (line[col] != '#') continue;
+                    _elves.Add(new Elf(col, row));
+                }
+            }
+        }
+
+        private IEnumerable<Direction> Directions()
+        {
+            Direction direction = _direction;
+            do
+            {
+                yield return direction;
+                direction = NextDirection(direction);
+            } while (direction != _direction);
+        }
+
+        public bool MoveElves()
+        {
+            // Collect proposals where all the elves would like to move
+            Dictionary<Elf, Elf> proposals = new();
+            Direction[] directions = Directions().ToArray();
+            foreach (Elf elf in _elves.Where(e => e.Neighbors().Any(_elves.Contains)))
+            {
+                foreach (Direction direction in directions)
+                {
+                    if (!elf.Neighbors(direction).Any(_elves.Contains))
+                    {
+                        proposals[elf] = elf.Neighbors(direction).First();
+                        break;
+                    }
+                }
+            }
+            
+            // Apply proposals that do not clash with other proposals
+            var uniqueProposals = proposals
+                .GroupBy(kv => kv.Value)
+                .Where(g => g.Count() == 1)
+                .Select(g => g.First())
+                .ToArray();
+            
+            foreach (var move in uniqueProposals)
+            {
+                _elves.Remove(move.Key);
+                _elves.Add(move.Value);
+            }
+            
+            // Start with another direction next time
+            _direction = NextDirection(_direction);
+
+            // Increment round counter and indicate whether any elf moved at all
+            Rounds += 1;
+            return uniqueProposals.Any();
+        }
+
+        public int CountEmptyTiles()
+        {
+            int xMin = _elves.Min(e => e.X);
+            int yMin = _elves.Min(e => e.Y);
+            int xMax = _elves.Max(e => e.X);
+            int yMax = _elves.Max(e => e.Y);
+
+            int size = (xMax - xMin + 1) * (yMax - yMin + 1);
+            return size - _elves.Count;
+        }
+    }
+    
+    public class Puzzle
+    {
+        private readonly Grove _grove;
+        
+        private Puzzle(string inputFile)
+        {
+            _grove = new Grove(inputFile);
+        }
+
+        private int ComputeSolution1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _grove.MoveElves();
+            }
+            return _grove.CountEmptyTiles();
+        }
+
+        private int ComputeSolution2()
+        {
+            while (_grove.MoveElves());
+            return _grove.Rounds;
+        }
+        
+        public static void Main()
+        {
+            var test = new Puzzle(@"D:\Advent\advent-of-csharp-2022\Day23\TestInput.txt");
+            Console.WriteLine($"Test solution 1: {test.ComputeSolution1()} (should be 110)");
+            Console.WriteLine($"Test solution 2: {test.ComputeSolution2()} (should be 20)");
+            
+            Console.WriteLine("---------------");
+            
+            var puzzle = new Puzzle(@"D:\Advent\advent-of-csharp-2022\Day23\PuzzleInput.txt");
+            Console.WriteLine($"Puzzle solution 1: {puzzle.ComputeSolution1()}");
+            Console.WriteLine($"Puzzle solution 2: {puzzle.ComputeSolution2()}");
+        }
+    }
+}
